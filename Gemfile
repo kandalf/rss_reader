50fa8e7e6cb77fe8215a46a84ac2a2f0a2fe84cb
Leonardo Mateo <leonardomateo@gmail.com>
2012-10-26 23:57:16 -0300

    Initial layout

---
+++ Gemfile
@@ -0,0 +1,26 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.8'
+gem 'pg'
+gem 'jquery-rails'
+gem 'twitter-bootstrap-rails'
+gem 'haml-rails'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+
+  gem 'therubyracer', :platforms => :ruby
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :development, :test do
+  gem 'debugger' 
+  gem 'unicorn'
+end
+
+group :test do
+ gem 'cucumber-rails'
+ gem 'database_cleaner'
+ gem 'rspec-rails'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RssReader::Application.config.secret_token = '958a0b2672ed56cfcb9a3c5967c691122120ba9d80c5eaadf2a0fd243a52fb628330662a416920895be42306d6ee3ef741625da2bcf2f89efbab6b5019c5796f'
